d0935f1defbc6a8333573915f37f845c259ad13b
NicoRamirez <nicoramirez72@yahoo.com>
2020-04-14 00:09:57 -0700

    new time

--- timeValue.py
+++ timeValue.py
@@ -1,15 +1,31 @@
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtCore import QThread
 import datetime, time
-value = 0
-intwert = 0
+import pytz
+
+#-------------------------------
+# Select Timezone
+#-------------------------------
+# TIME_ZONE = "US/Alaska"
+# TIME_ZONE = "US/Arizona"
+# TIME_ZONE = "US/Central"
+# TIME_ZONE = "US/East-Indiana"
+# TIME_ZONE = "US/Eastern"
+# TIME_ZONE = "US/Hawaii"
+# TIME_ZONE = "US/Indiana-Starke"
+# TIME_ZONE = "US/Michigan"
+TIME_ZONE = "US/Mountain"
+# TIME_ZONE = "US/Pacific"
+# TIME_ZONE = "US/Samoa"
+#-------------------------------
+#-------------------------------
+#-------------------------------
 
 class Controller(QThread, object):
     
-    now = datetime.datetime.now()
-    timeInterval="%0.2d:%0.2d" %(now.hour, now.minute)
-    
     newTime = pyqtSignal(object)
+    fmt = '%H:%M'
+
     def __init__(self, event):
         QThread.__init__(self)
         self.stopped = event
@@ -21,9 +37,10 @@ class Controller(QThread, object):
     def inTime1(self):
         global timeInterval
         now = datetime.datetime.now()
-        timeInterval="%0.2d:%0.2d" %(now.hour, now.minute)
-        # ~ timeInterval= datetime.datetime.now().strftime('%d %B %Y %I:%M:%S')
-        # ~ timeInterval= datetime.datetime.now().strftime('%I:%M:%S %p')
+        
+        timezone = pytz.timezone(TIME_ZONE)
+        test = datetime.datetime.now(timezone)
+        timeInterval = test.strftime(self.fmt)
         
         self.newTime.emit(timeInterval)
    

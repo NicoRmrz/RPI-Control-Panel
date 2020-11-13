6d2988f23d5c812173b7c974f5b063abf2a40db7
Nico Ramirez <nicoramirez72@yahoo.com>
2020-11-13 09:02:51 -0800

    hide cursor

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from PyQt5.QtWidgets import QApplication
 from mainWindow import MainWindow
+from PyQt5.QtCore import Qt
 
 if __name__== "__main__":
     import sys
     app = QApplication(sys.argv)
+    app.setOverrideCursor(Qt.BlankCursor)
     ui = MainWindow()
     ui.showFullScreen()
     # ~ ui.show()
